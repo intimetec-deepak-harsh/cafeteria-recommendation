35dbdb7451aa164e7a0f5f11de8d2057e6bd623b
Deepak Harsh <deepak.harsh@intimetec.com>
2024-06-21 14:04:54 +0530

    start with other user crud

--- server/src/services/recommendationEngine.ts
+++ server/src/services/recommendationEngine.ts
@@ -1,15 +1,4 @@
-import { Socket } from 'socket.io';
-import UserService from '../controller/authController';
-import { db } from '../database/connection';
-import { RowDataPacket } from 'mysql2/promise';
 
 
-
-
-class RecommendationEngineService {
-
-    
   
-}
 
-export default RecommendationEngineService;
